d67999b4f62f1bd8a4bd8915a883fd11837be603
YouHyungChul <61854958+YouHyungChul@users.noreply.github.com>
2020-03-19 12:10:08 +0900

    2번째 프레그먼트 만들고 토스트 활용

--- app/src/main/java/kr/co/tjoeun/a20200319_01_fragment01/fragments/SecondFragment.java
+++ app/src/main/java/kr/co/tjoeun/a20200319_01_fragment01/fragments/SecondFragment.java
@@ -4,24 +4,49 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import kr.co.tjoeun.a20200319_01_fragment01.R;
+import kr.co.tjoeun.a20200319_01_fragment01.databinding.FragmentSecondBinding;
 
 public class SecondFragment extends Fragment {
+    FragmentSecondBinding binding = null;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_second, container, false  );
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_second, container, false);
+        return  binding.getRoot();
 
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        binding.loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String id = binding.idEdt.getText().toString();
+                String pw = binding.pwEdt.getText().toString();
+                if (id.equals("admin") && pw.equals("asdf1234")) {
+                    Toast.makeText(getActivity(), "로그인성공", Toast.LENGTH_SHORT).show();
+                    }else{
+                    if (id.equals("admin")){
+                        Toast.makeText(getActivity(), "비밀번호는 틀렸습니다.", Toast.LENGTH_SHORT).show();
+                    }else{
+                        {
+                            Toast.makeText(getActivity(), "아이디는 틀렸습니다.", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                }
+            }
+        });
     }
 }
